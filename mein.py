0801efeee78223444ca4638b35b7aa1d6fa9ac08
A06 <160205029+Alishba06@users.noreply.github.com>
2025-03-01 10:38:05 +0500

    Added mein.py file

---
+++ mein.py
@@ -0,0 +1,138 @@
+
+# Data Type  List:
+
+# Numeric:
+     #. Integers
+     #.  Float
+     #.  Complex Number
+
+# Sequence Type:
+
+    #. String
+    #. List
+    #. Tuple
+    #. range
+
+# Mapping Type
+
+   # Dicionary
+
+# Set Types
+
+  #. Set
+  #.Frozenset
+
+# Boolean Type
+
+  #. bool:
+     #. True
+     #.  False
+
+
+# Binary Types
+
+   #. beytes
+   #. bytearray
+   #. memoryview
+
+# None Type
+   
+   #.None
+   
+
+
+
+# Sequence Type
+
+a = "Hello world"  # String
+
+print(a)
+
+b =  ["black" , "purple" , "pink"]   # List
+
+print(b)
+
+c = ("black" , "purple" , "pink")    # Tuple
+
+print(c)
+
+
+for num in range(6):  # range
+ print(num)
+
+
+
+#   Numeric Types
+
+a = 123    # Integers
+
+print(a)
+
+b = 23.5   # Float
+
+print(b)
+ 
+c = 123j   # Complex
+
+print(c)      
+
+
+
+# Mapping Type
+
+a = {'name':'alishba','age':'17','city':'TandoAllayer'}  # dictionaries 
+
+print(a)
+
+
+
+
+
+# Set Types
+
+colors = {"black" , "pink" , "pink" , "purple"}  # set type
+
+print(colors)
+
+
+number = frozenset([1 , 2 , 2 , 3 , 4 , 5])  # frozenset
+
+print(number)
+
+
+
+# Boolen Types
+
+a = True
+
+b = False
+
+
+
+# Binary Types
+
+a = b'hello'  # bytes type
+
+print(a)
+
+
+b = bytearray(5)
+
+print(b)  # bytearray
+
+
+c = memoryview(bytes(5))  # memoryview
+
+
+# None Type
+
+a = None
+
+print(a)
+
+
+
+
+
+
+
